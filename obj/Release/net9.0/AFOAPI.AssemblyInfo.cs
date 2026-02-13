9437497cdb2fe5a14f7b6928a4501744af8487cf
Eli7273 <eliasafgonzalez0@gmail.com>
2026-02-13 13:59:16 -0800

    api

--- obj/Release/net9.0/AFOAPI.AssemblyInfo.cs
+++ obj/Release/net9.0/AFOAPI.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AFOAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae80c5994154676e5a0617bd5e40e91b7c95c908")]
 [assembly: System.Reflection.AssemblyProductAttribute("AFOAPI")]
