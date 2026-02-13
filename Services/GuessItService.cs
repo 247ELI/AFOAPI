47e66002b4f50cc6ecbe704ea01ea84893039926
Eli7273 <eliasafgonzalez0@gmail.com>
2026-02-13 14:00:44 -0800

    changes

--- Services/GuessItService.cs
+++ Services/GuessItService.cs
@@ -9,13 +9,13 @@ namespace AFOAPI.Services
     {
         private static readonly System.Random _rand = new System.Random();
 
-        private static readonly int _secret = _rand.Next(1, 28);
+        private static readonly int _secret = _rand.Next(1, 20);
 
         public string Guess(int guess)
         {
-            if (guess < 1 || guess > 27)
+            if (guess < 1 || guess > 20)
             {
-                return "Guess must be between 1 and 27.";
+                return "Guess must be between 1 and 20.";
             }
 
             if (guess < _secret) return "Too low!";
